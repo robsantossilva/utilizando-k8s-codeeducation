b57e8f9c53931d7db3bead229aaac8081d37fa46
Robson Silva <robsantossilva@gmail.com>
2020-11-24 15:16:18 -0300

    Ajuste cloubuild.yaml

--- src/desafio-go/main.go
+++ src/desafio-go/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, greeting("Code.education Rocks!"))
+}
 
 func main() {
-	fmt.Println(greeting("Code.education Rocks!"))
+	http.HandleFunc("/", index)
+	fmt.Println("Server is up and listening on port 8000.")
+	http.ListenAndServe(":8000", nil)
 }
 
 func greeting(txt string) string {
